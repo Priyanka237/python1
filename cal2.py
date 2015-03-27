53d5f4302f80df1a0918d18808b18394657a7c55
Priyanka237 <pdghori237@gmail.com>
2015-03-27 11:32:21 +0530

    first commit

--- cal2.py
+++ cal2.py
@@ -9,29 +9,24 @@ choice = int(input("enter your choice"))
 while choice:
     if choice == 1:
         s = a + b
-        print('sum is:')
-        print(s)
-        choice = int(input("enter your choice"))
+        print('sum is:',s)
+        break
     elif choice == 2:
         s1 = a - b
-        print('sub is:')
-        print(s1)
-        choice = int(input("enter your choice"))
+        print('sub is:',s1)
+        break
     elif choice == 3:
         m = a * b
-        print('mul is:')  
-        print(m)
-        choice = int(input("enter your choice"))
+        print('mul is:',m)  
+        break   
     elif choice == 4:
         d = a / b
-        print('div is:')
-        print(d)
-        choice = int(input("enter your choice"))
+        print('div is:',d)
+        break
     elif choice == 5:
         m1 = a % b
-        print('mod is:')
-        print(m1)
-        choice = int(input("enter your choice"))
+        print('mod is:',m1)
+        break
     else:
         print('nothing')
         break
